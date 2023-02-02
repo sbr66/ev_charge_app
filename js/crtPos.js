51e299e3aa8e4a3b520daa5ed1110483e3b8d364
sbr66 <bonam22@gmail.com>
2023-02-02 10:45:32 +0900

    지도, 마커 생성

---
+++ js/crtPos.js
@@ -0,0 +1,28 @@
+var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0,
+};
+
+function success(pos) {
+  let crd = pos.coords;
+  //좌표를 알아낼 수 있는데, 여기서 알아낸 좌표를 kakaoAPI url에 사용할 것이다.
+  //   console.log("위도 : " + position.coords.latitude);
+  //   console.log("경도: " + position.coords.longitude);
+  let lat = crd.latitude;
+  let lon = crd.longitude;
+
+  if (typeof getCrtNm === "function") {
+    getCrtNm(lat, lon); // getCrt.js
+  }
+
+  if (typeof getCrtLists === "function") {
+    getCrtLists(lat, lon); // getCrtLists.js
+  }
+}
+
+function error(err) {
+  console.warn("ERROR(" + err.code + "): " + err.message);
+}
+
+navigator.geolocation.getCurrentPosition(success, error, options);
